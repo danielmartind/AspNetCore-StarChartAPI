49cea5bca5d320aa327c4801c95b163c02f4dc3b
Dan Martin <dan.martin@solidroute.co.uk>
2021-04-22 17:14:15 +0100

    add get methods

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using StarChart.Data;
 
 namespace StarChart.Controllers
@@ -17,6 +18,39 @@ namespace StarChart.Controllers
         public CelestialObjectController(ApplicationDbContext context)
         {
             _context = context;
+        }
+
+        [HttpGet("{id:int}", Name = "GetById")]
+        public IActionResult GetById(int id)
+        {
+            var co = _context.CelestialObjects.FirstOrDefault(c => c.Id == id);
+            if (co == null) return NotFound();
+            co.Satellites = _context.CelestialObjects.Where(s => s.OrbitedObjectId == co.Id).ToList();
+            return Ok(co);
+        }
+
+        [HttpGet("{name}")]
+        public IActionResult GetByName(string name)
+        {
+            var objects = _context.CelestialObjects.Where(c => c.Name == name);
+            if (!objects.Any()) return NotFound();
+            foreach (var celestialObject in objects)
+            {
+                celestialObject.Satellites = _context.CelestialObjects.Where(s => s.OrbitedObjectId == celestialObject.Id).ToList();
+            }
+            return Ok(objects);
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var objects = _context.CelestialObjects.ToList();
+            foreach (var celestialObject in objects)
+            {
+                celestialObject.Satellites = _context.CelestialObjects.Where(s => s.OrbitedObjectId == celestialObject.Id).ToList();
+            }
+
+            return Ok(objects);
         }
     }
 }
